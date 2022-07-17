a9b7dd284342af2bd1c8756819fdf8690737b152
Vinicius <comercial.viniciusgabriel@gmail.com>
2022-07-16 21:13:46 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,14 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
   return (
-    <h1>Hello, World!</h1>
-  )
+    <>
+      <h1>Hello, World!</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  );
 }
 
 export default App
