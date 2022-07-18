37a2f409cd2a7cc5173c0daf661880a8b9c03577
Vinicius <comercial.viniciusgabriel@gmail.com>
2022-07-17 22:01:46 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,22 @@
-import Header from "./components/Header"
-import SalesCard from "./components/SalesCard"
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
     <>
+    <ToastContainer />
       <Header />
       <main>
         <section id="sales">
           <div className="dsmeta-container">
-        <SalesCard />
-        </div>
+            <SalesCard />
+          </div>
         </section>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,23 +1,24 @@
-import axios from 'axios';
-import icon from '../../assets/img/notification-icon.svg'
-import { BASE_URL } from '../../utils/request';
-import './styles.css'
+import axios from "axios";
+import { toast } from "react-toastify";
+import icon from "../../assets/img/notification-icon.svg";
+import { BASE_URL } from "../../utils/request";
+import "./styles.css";
 
 type Props = {
   saleId: number;
+};
+function handleClick(id: number) {
+  axios(`${BASE_URL}/sales/${id}/notification`).then((response) => {
+    toast.info("SMS enviado com sucesso!");
+  });
 }
-function handleClick(id: number){
-axios(`${BASE_URL}/sales/${id}/notification`)
-  .then(response => ("SUCESSO"))
 
-}
-
-function NotificationButton({saleId} : Props) {
+function NotificationButton({ saleId }: Props) {
   return (
     <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
       <img src={icon} alt="Notificar" />
     </div>
-  )
+  );
 }
 
 export default NotificationButton;
